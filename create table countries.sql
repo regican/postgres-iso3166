d3ad51f246a21bc0216228ef1885a27192f8f2ff
regican <regican@gmail.com>
2013-02-27 16:00:23 -0500

    initial commit

---
+++ create table countries.sql
@@ -0,0 +1,6 @@
+﻿-- Table structure for table countries  --
+
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE countries (country_id smallint NOT NULL,country_name VARCHAR(100) DEFAULT NULL,country_code_char2 varchar(2) NOT NULL,country_code_char3 varchar(3) NOT NULL,un_region varchar(80) DEFAULT NULL,un_subregion varchar(80) DEFAULT NULL,PRIMARY KEY (country_id)
+);
---
+++ create table states_subdivisions.sql
@@ -0,0 +1,5 @@
+﻿-- Table structure for table states_subdivisions  --
+
+DROP TABLE IF EXISTS states_subdivisions;
+
+CREATE TABLE states_subdivisions (state_subdivision_id smallint NOT NULL,country_code_char2 varchar(2) NOT NULL,country_code_char3 varchar(3) NOT NULL,state_subdivision_name varchar(80) DEFAULT NULL,state_subdivision_alternate_names varchar(200) DEFAULT NULL,primary_level_name varchar(80) DEFAULT NULL,state_subdivision_code varchar(50) NOT NULL,PRIMARY KEY(state_subdivision_id));
